5c0a23a1f101bdd3912326ae2071129d6fdc7d09
mcanat-polarflows <mert.canat@polarflows.com>
2025-09-14 22:03:17 +0200

    wip

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -78,9 +78,9 @@ export const VALIDATION_CONFIG = {
 };
 
 export const SERVICE_WORKER_CONFIG = {
-  CACHE_VERSION: 'v1.0.43',
-  STATIC_CACHE_NAME: 'polar-flows-static-v1.0.43',
-  DYNAMIC_CACHE_NAME: 'polar-flows-dynamic-v1.0.43',
+  CACHE_VERSION: 'v1.0.45',
+  STATIC_CACHE_NAME: 'polar-flows-static-v1.0.45',
+  DYNAMIC_CACHE_NAME: 'polar-flows-dynamic-v1.0.45',
 };
 
 export const CONTACT_CONFIG = {
